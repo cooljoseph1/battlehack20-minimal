71ee52b556b99950b0631522300537a3c87c6e92
cooljoseph1 <camacho.joseph@gmail.com>
2020-04-18 23:44:49 -0600

    Change version number to 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Read more at the Battlehack website: https://bh2020.battlecode.org.
 """
 
 setup(name='battlehack20-minimal',
-      version="1.0.2",
+      version="1.0.3",
       description='Battlehack 2020 fancy viewer.',
       author='cooljoseph',
       long_description=long_description,
