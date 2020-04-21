e7a7c14d8f0b6da2c1ea7827ee694c7006d26790
cooljoseph1 <camacho.joseph@gmail.com>
2020-04-21 10:14:12 -0600

    Updated basic viewer to actually work

--- battlehack20/engine/game/robot.py
+++ battlehack20/engine/game/robot.py
@@ -8,3 +8,11 @@ class Robot:
         self.id = id
         self.type = type
         self.runner = runner
+
+    def __str__(self):
+        team = 'B' if self.team.value else 'W'
+        return '%s%3d' % (team, self.id)
+
+    def __repr__(self):
+        team = 'BLACK' if self.team.value else 'WHITE'
+        return f'<ROBOT {self.id} {team}>'
